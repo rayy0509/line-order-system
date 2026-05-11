763c3f92c719b00917617d17dc89ae270f994bae
Ray <lin8392637@gmail.com>
2026-05-12 03:06:22 +0800

    connect mysql

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ require('dotenv').config();
 const express = require('express');
 const axios = require('axios');
 const path = require('path');
+const db = require('./db');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
